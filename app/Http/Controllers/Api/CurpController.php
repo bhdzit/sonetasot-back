18483842eaa6949860778cbbff43370be1eb3929
Bryan Hernandez <bryan.hernandez@mtpinternational.mx>
2023-08-05 13:35:31 -0600

    Se creo regla de validcion de curp

--- app/Http/Controllers/Api/CurpController.php
+++ app/Http/Controllers/Api/CurpController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CurpStoreRequest;
 use App\Models\Curp;
+
+
 
 /**
 * @OA\Info(title="API Usuarios", version="1.0")
@@ -28,5 +31,11 @@ class CurpController extends Controller
     public function index()
     {
         return Curp::all();
+    }
+    
+    public function create(CurpStoreRequest $request)
+    {
+       $curp = Curp::create($request.array());
+       return $curp;
     }
 }
