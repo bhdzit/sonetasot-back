3b48c03fa9bb5c99c330dcc4e771b0fda0473326
Bryan Hernandez <bryan.hernandez@mtpinternational.mx>
2023-08-05 13:37:07 -0600

    Se crea tabla de curps

--- database/migrations/2023_08_05_042832_create_curps_table.php
+++ database/migrations/2023_08_05_042832_create_curps_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->date('fecha_nacimiento');
             $table->string('sexo');
             $table->string('estado');
+            $table->string('curp');
             $table->timestamps();
         });
     }
